da31bec1748173f93707d5e4f2ee8a9a714ba89f
ErikEJ <ejlskov@hotmail.com>
2018-06-19 21:43:11 +0200

    Add AsSqlScript extension method

--- src/GUI/ErikEJ.EntityFrameworkCore.DgmlBuilder/ModelExtension.cs
+++ src/GUI/ErikEJ.EntityFrameworkCore.DgmlBuilder/ModelExtension.cs
@@ -1,7 +1,12 @@
 ﻿using System;
 using System.IO;
 using System.Reflection;
+using System.Text;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Storage;
 
 // ReSharper disable once CheckNamespace
 namespace Microsoft.EntityFrameworkCore
@@ -19,10 +24,37 @@ namespace Microsoft.EntityFrameworkCore
             return dgml;
         }
 
+        public static string AsSqlScript(this DbContext context)
+        {
+            return GenerateCreateScript(context);
+        }
+
         private static string CreateDebugView(DbContext context)
         {
             var model = context.Model;
             return model.AsModel().DebugView.View;
+        }
+
+        private static string GenerateCreateScript(DbContext dbContext)
+        {
+            var database = dbContext.Database;
+            var model = database.GetService<IModel>();
+            var differ = database.GetService<IMigrationsModelDiffer>();
+            var generator = database.GetService<IMigrationsSqlGenerator>();
+            var sql = database.GetService<ISqlGenerationHelper>();
+
+            var operations = differ.GetDifferences(null, model);
+            var commands = generator.Generate(operations, model);
+
+            var builder = new StringBuilder();
+            foreach (var command in commands)
+            {
+                builder
+                    .Append(command.CommandText)
+                    .AppendLine(sql.BatchTerminator);
+            }
+
+            return builder.ToString();
         }
 
         private static string GetTemplate()
