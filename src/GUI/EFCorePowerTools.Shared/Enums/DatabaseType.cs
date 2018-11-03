1b7767468d75bb033bf77399d0029edbc79e4b9b
Stefan Over <herdosratos@googlemail.com>
2018-11-03 23:20:18 +0100

    Added DatabaseConnectionModel and DatabaseDefinitionModel, including tests, added shared enum DatabaseType

---
+++ src/GUI/EFCorePowerTools.Shared/Enums/DatabaseType.cs
@@ -0,0 +1,14 @@
+﻿// ReSharper disable InconsistentNaming
+// ReSharper disable IdentifierTypo
+namespace EFCorePowerTools.Shared.Enums
+{
+    public enum DatabaseType
+    {
+        Undefined = 0,
+        SQLCE35 = 1,
+        SQLCE40 = 2,
+        SQLServer = 3,
+        SQLite = 4,
+        Npgsql = 5
+    }
+}
---
+++ src/GUI/EFCorePowerTools.Shared/Models/DatabaseDefinitionModel.cs
@@ -0,0 +1,36 @@
+﻿namespace EFCorePowerTools.Shared.Models
+{
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
+    using Annotations;
+
+    /// <summary>
+    /// A model holding data about a database definition, e.g. a DacPac file or database project.
+    /// </summary>
+    public class DatabaseDefinitionModel : INotifyPropertyChanged
+    {
+        private string _filePath;
+
+        /// <summary>
+        /// Gets or sets the file path to the database definition.
+        /// </summary>
+        public string FilePath
+        {
+            get => _filePath;
+            set
+            {
+                if (value == _filePath) return;
+                _filePath = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
