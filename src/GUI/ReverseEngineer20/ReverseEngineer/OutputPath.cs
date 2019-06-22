b4f15987fd285c304e4cefc3cf0223c6f0e1aacc
Rene Koch <rene.koch@swd-duesseldorf.de>
2019-06-22 22:02:46 +0200

    Checked in missing file.

---
+++ src/GUI/ReverseEngineer20/ReverseEngineer/OutputPath.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseEngineer20.ReverseEngineer
+{
+    public class OutputPath
+    { 
+        public string Models { get; set; }
+
+        public string Context { get; set; }
+    }
+}
