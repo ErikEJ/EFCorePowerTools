5fb759b549cb58e79e31747e874407135e297ff3
ErikEJ <ejlskov@hotmail.com>
2019-08-22 16:15:22 +0200

    Try to fix assembly load issue

--- src/GUI/EFCorePowerTools/Properties/AssemblyInfo.cs
+++ src/GUI/EFCorePowerTools/Properties/AssemblyInfo.cs
@@ -12,5 +12,5 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.3.0.0")]
+[assembly: AssemblyFileVersion("2.3.0.0")]
