4cebf23bf219746b4fa8fad7b6aa7ae11354a6e7
ErikEJ <ejlskov@hotmail.com>
2019-11-17 15:08:14 +0100

    Fix test

--- src/GUI/UnitTests/DacpacTest.cs
+++ src/GUI/UnitTests/DacpacTest.cs
@@ -31,7 +31,7 @@ namespace UnitTests
             var result = builder.GetTableDefinitions();
 
             // Assert
-            Assert.AreEqual("dbo.Album", result[0].Name);
+            Assert.AreEqual("[dbo].[Album]", result[0].Name);
             Assert.AreEqual(11, result.Count);
         }
 
