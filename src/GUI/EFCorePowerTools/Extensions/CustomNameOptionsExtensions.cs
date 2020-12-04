c1439fe819e0e9e6ed9ea151914a61d71bea715e
Erik Ejlskov Jensen <ErikEJ@users.noreply.github.com>
2020-12-04 11:20:46 +0100

    Return potentially changed renaming path (#631)
    * Return potentially changed renaming path
    
    Update reveng tool
    
    * Always return file name

--- src/GUI/EFCorePowerTools/Extensions/CustomNameOptionsExtensions.cs
+++ src/GUI/EFCorePowerTools/Extensions/CustomNameOptionsExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using ReverseEngineer20.ReverseEngineer;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Runtime.Serialization.Json;
@@ -8,7 +9,7 @@ namespace EFCorePowerTools.Extensions
 {
     public class CustomNameOptionsExtensions
     {
-        public static List<Schema> TryRead(string optionsCustomNamePath, string optionsPath)
+        public static Tuple<List<Schema>, string> TryRead(string optionsCustomNamePath, string optionsPath)
         {
             if (!optionsPath.EndsWith("efpt.config.json", System.StringComparison.OrdinalIgnoreCase))
             {
@@ -21,14 +22,14 @@ namespace EFCorePowerTools.Extensions
                 }
             }
 
-            if (!File.Exists(optionsCustomNamePath)) return null;
-            if (File.Exists(optionsCustomNamePath + ".ignore")) return null;
+            if (!File.Exists(optionsCustomNamePath)) return new Tuple<List<Schema>, string>(null, optionsCustomNamePath);
+            if (File.Exists(optionsCustomNamePath + ".ignore")) return new Tuple<List<Schema>, string>(null, optionsCustomNamePath);
 
             using (var ms = new MemoryStream(Encoding.UTF8.GetBytes(File.ReadAllText(optionsCustomNamePath, Encoding.UTF8))))
             {
                 var ser = new DataContractJsonSerializer(typeof(List<Schema>));
                 var customNamingOptions = ser.ReadObject(ms) as List<Schema>;
-                return customNamingOptions;
+                return new Tuple<List<Schema>, string>(customNamingOptions, optionsCustomNamePath);
             }
         }
     }
