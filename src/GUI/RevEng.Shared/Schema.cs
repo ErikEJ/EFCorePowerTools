c833417fe44c9921a97fcdd5b86185c0b4cfe5af
stefano mostarda <stesa98@hotmail.com>
2020-12-09 23:12:07 +0100

    Correctly return renamed objects

--- src/GUI/RevEng.Shared/Schema.cs
+++ src/GUI/RevEng.Shared/Schema.cs
@@ -1,25 +1,34 @@
 ﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
 
 namespace ReverseEngineer20.ReverseEngineer
 {
+    [DataContract]
     public class Schema
     {
         public Schema()
         {
             Tables = new List<TableRenamer>();
         }
+
+        [DataMember]
         public bool UseSchemaName { get; set; }
 
+        [DataMember(EmitDefaultValue = false, IsRequired = false)]
         public string SchemaName { get; set; }
 
         public List<TableRenamer> Tables { get; set; }
 
+        [DataMember(EmitDefaultValue = false, IsRequired = false)]
         public string TableRegexPattern { get; set; }
 
+        [DataMember(EmitDefaultValue = false, IsRequired = false)]
         public string TablePatternReplaceWith { get; set; }
 
+        [DataMember(EmitDefaultValue = false, IsRequired = false)]
         public string ColumnRegexPattern { get; set; }
 
+        [DataMember(EmitDefaultValue = false, IsRequired = false)]
         public string ColumnPatternReplaceWith { get; set; }
     }
 }
