81d7eb43eb29f6c0dd144085f86c93a09d74e051
Olivier MATROT <omatrot@free.fr>
2020-12-21 16:31:23 +0100

    Started testing edmx to DatabaseModel

--- src/GUI/ErikJ.EntityFrameworkCore.Edmx/Scaffolding/EdmxDatabaseModelFactory.cs
+++ src/GUI/ErikJ.EntityFrameworkCore.Edmx/Scaffolding/EdmxDatabaseModelFactory.cs
@@ -1,10 +1,94 @@
 ﻿using System;
+using System.Data.Common;
+using System.IO;
+using System.Linq;
+using LinqToEdmx;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.EntityFrameworkCore.Scaffolding;
+using Microsoft.EntityFrameworkCore.Scaffolding.Metadata;
+
 namespace ErikJ.EntityFrameworkCore.Edmx.Scaffolding
 {
     public class EdmxDatabaseModelFactory : IDatabaseModelFactory
     {
-        public EdmxDatabaseModelFactory()
+        private readonly IDiagnosticsLogger<DbLoggerCategory.Scaffolding> _logger;
+
+        public EdmxDatabaseModelFactory(IDiagnosticsLogger<DbLoggerCategory.Scaffolding> logger)
         {
+            _logger = logger;
+        }
+
+        internal string GetSchemaNameV3(EdmxV3 edmxv3, string entityName)
+        {
+            var entitySetContainer = edmxv3.GetItems<LinqToEdmx.Model.StorageV3.EntityContainer>();
+            foreach (var container in entitySetContainer)
+            {
+                foreach (var entitySet in container.EntitySets.Where(es => es.Type == "Tables"))
+                {
+                    if (entitySet.Name == entityName)
+                    {
+                        return entitySet.Schema;
+                    }
+                }
+
+                // FIXME Return the correct schema for views
+                //foreach (var entitySet in container.EntitySets.Where(es => es.Type == "Views"))
+                //{
+                //    if (entitySet.EntityType.Contains(entityName))
+                //    {
+                //        return entitySet.Schema;
+                //    }
+                //}
+            }
+            return string.Empty;
+        }
+
+        public DatabaseModel Create(string edmxPath, DatabaseModelFactoryOptions options)
+        {
+            if (string.IsNullOrEmpty(edmxPath))
+            {
+                throw new ArgumentException(@"invalid path", nameof(edmxPath));
+            }
+            if (!File.Exists(edmxPath))
+            {
+                throw new ArgumentException($"Edmx file not found: {edmxPath}");
+            }
+
+            var schemas = options.Schemas;
+            var tables = options.Tables;
+
+            var dbModel = new DatabaseModel
+            {
+                DatabaseName = Path.GetFileNameWithoutExtension(edmxPath),
+                DefaultSchema = schemas.Count() > 0 ? schemas.First() : "dbo"
+            };
+
+            // Assume EDMX version is 3.0
+            var edmxv3 = LinqToEdmx.EdmxV3.Load(edmxPath);
+
+            if (string.Compare(edmxv3.Version, @"3.0", StringComparison.InvariantCultureIgnoreCase) == 0)
+            {
+
+
+                var items = edmxv3.GetItems<LinqToEdmx.Model.ConceptualV3.EntityType>();
+
+                foreach (var item in items)
+                {
+                    var dbTable = new DatabaseTable
+                    {
+                        Name = item.Name,
+                        Schema = GetSchemaNameV3(edmxv3, item.Name),
+                    };
+                }
+            }
+
+            return dbModel;
+        }
+
+        public DatabaseModel Create(DbConnection connection, DatabaseModelFactoryOptions options)
+        {
+            throw new NotImplementedException();
         }
     }
 }
---
+++ src/GUI/NUnitTestCore/EdmxTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using ErikJ.EntityFrameworkCore.Edmx.Scaffolding;
+using Microsoft.EntityFrameworkCore.Scaffolding;
+using NUnit.Framework;
+
+namespace NUnitTestCore
+{
+    [TestFixture]
+    public class EdmxTest
+    {
+        private string edmx;
+
+        [SetUp]
+        public void Setup()
+        {
+            edmx = TestPath("AdventureWorks2019.edmx");
+        }
+
+        [Test]
+        public void Issue551()
+        {
+            // Arrange
+            var factory = new EdmxDatabaseModelFactory(null);
+            var options = new DatabaseModelFactoryOptions(null, new List<string>());
+
+            // Act
+            var dbModel = factory.Create(TestPath("AdventureWorks2019.edmx"), options);
+
+            // Assert
+            Assert.AreEqual(1, dbModel.Tables.Count());
+        }
+
+        private string TestPath(string file)
+        {
+            return Path.Combine(TestContext.CurrentContext.TestDirectory, file);
+        }
+    }
+}
