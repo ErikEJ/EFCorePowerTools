495ce4556e9949d340f603bcd34524e4fc3d16e4
Olivier MATROT <omatrot@free.fr>
2021-01-25 16:21:37 +0100

    Progress on foreign keys from SSDL

--- src/GUI/ErikJ.EntityFrameworkCore.Edmx/Scaffolding/EdmxDatabaseModelFactory.cs
+++ src/GUI/ErikJ.EntityFrameworkCore.Edmx/Scaffolding/EdmxDatabaseModelFactory.cs
@@ -1,7 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Data.Common;
 using System.IO;
 using System.Linq;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.XPath;
 using LinqToEdmx;
 using LinqToEdmx.MapV3;
 using LinqToEdmx.Model.ConceptualV3;
@@ -12,8 +16,18 @@ using Microsoft.EntityFrameworkCore.Scaffolding.Metadata;
 
 namespace ErikJ.EntityFrameworkCore.Edmx.Scaffolding
 {
+    // This is the storage side of the scaffolding logic
+    // Because the EDMX format was not designed to allow the database creation, the SSDL part of the schema won't contain default values, extended properties, primary keys, unique constraints and index informations.
     public class EdmxDatabaseModelFactory : IDatabaseModelFactory
     {
+        // SQL Server db types
+        private static readonly ISet<string> SQLServerDateTimePrecisionTypes = new HashSet<string> { "datetimeoffset", "datetime2", "time" };
+
+        private static readonly ISet<string> SQLServerMaxLengthRequiredTypes
+            = new HashSet<string> { "binary", "varbinary", "char", "varchar", "nchar", "nvarchar" };
+
+        // FIXME Add PostgreSQL db types
+
         private readonly IDiagnosticsLogger<DbLoggerCategory.Scaffolding> _logger;
 
         public EdmxDatabaseModelFactory(IDiagnosticsLogger<DbLoggerCategory.Scaffolding> logger)
@@ -21,6 +35,7 @@ namespace ErikJ.EntityFrameworkCore.Edmx.Scaffolding
             _logger = logger;
         }
 
+        // FIXME There could be a table or a view with the same name in multiple schema /!\
         internal string GetSchemaNameV3(EdmxV3 edmxv3, string entityName)
         {
             var entitySetContainer = edmxv3.GetItems<LinqToEdmx.Model.StorageV3.EntityContainer>();
@@ -30,20 +45,27 @@ namespace ErikJ.EntityFrameworkCore.Edmx.Scaffolding
                 {
                     if (entitySet.Name == entityName)
                     {
-                        return entitySet.Schema;
+                        if (string.IsNullOrEmpty(entitySet.Schema1) && string.IsNullOrEmpty(entitySet.Schema))
+                        {
+                            throw new InvalidOperationException(@$"[{entityName}] schema could not be null. This usually indicates a bug");
+                        }
+                        return entitySet.Schema ?? entitySet.Schema1;
                     }
                 }
 
-                // FIXME Return the correct schema for views
-                //foreach (var entitySet in container.EntitySets.Where(es => es.Type == "Views"))
-                //{
-                //    if (entitySet.EntityType.Contains(entityName))
-                //    {
-                //        return entitySet.Schema;
-                //    }
-                //}
+                foreach (var entitySet in container.EntitySets.Where(es => es.Type == "Views"))
+                {
+                    if (entitySet.EntityType.Contains(entityName))
+                    {
+                        if (string.IsNullOrEmpty(entitySet.Schema1) && string.IsNullOrEmpty(entitySet.Schema))
+                        {
+                            throw new InvalidOperationException(@$"[{entityName}] schema could not be null. This usually indicates a bug");
+                        }
+                        return entitySet.Schema ?? entitySet.Schema1;
+                    }
+                }
             }
-            return string.Empty;
+            throw new InvalidOperationException(@$"Unable to identify the database object schema for entity [{entityName}]. This usually indicates a bug");
         }
 
         public DatabaseModel Create(string edmxPath, DatabaseModelFactoryOptions options)
@@ -66,12 +88,17 @@ namespace ErikJ.EntityFrameworkCore.Edmx.Scaffolding
                 DefaultSchema = schemas.Count() > 0 ? schemas.First() : "dbo"
             };
 
-            // FIXME Assume EDMX version is 3.0 for now
-            var edmxv3 = LinqToEdmx.EdmxV3.Load(edmxPath);
+            // Detect the EDMX file version
+            XDocument edmxFile = XDocument.Load(edmxPath);
 
-            if (string.Compare(edmxv3.Version, @"3.0", StringComparison.InvariantCultureIgnoreCase) == 0)
+            var edmxVersion = ((XElement)edmxFile.FirstNode).FirstAttribute.Value;
+
+            // FIXME Assume EDMX version is 3.0 for now
+
+            if (string.Compare(edmxVersion, @"3.0", StringComparison.InvariantCultureIgnoreCase) == 0)
             {
-                var items = edmxv3.GetItems<LinqToEdmx.Model.ConceptualV3.EntityType>();
+                var edmxv3 = LinqToEdmx.EdmxV3.Load(edmxPath);
+                var items = edmxv3.GetItems<LinqToEdmx.Model.StorageV3.EntityTypeStore>();
 
                 foreach (var item in items)
                 {
@@ -81,10 +108,19 @@ namespace ErikJ.EntityFrameworkCore.Edmx.Scaffolding
                         Schema = GetSchemaNameV3(edmxv3, item.Name),
                     };
 
-                    GetColumns(item, dbTable/*, typeAliases, model.GetObjects<TSqlDefaultConstraint>(DacQueryScopes.UserDefined).ToList()*/, edmxv3);
-                    GetPrimaryKey(item, dbTable);
+                    GetColumnsV3(item, dbTable/*, typeAliases, model.GetObjects<TSqlDefaultConstraint>(DacQueryScopes.UserDefined).ToList()*/, edmxv3);
+                    GetPrimaryKeyV3(item, dbTable);
 
                     dbModel.Tables.Add(dbTable);
+                }
+
+                foreach (var item in items)
+                {
+                    GetForeignKeysV3(edmxv3, item, dbModel);
+                    // Unique constraints are not available in the model
+                    // GetUniqueConstraints(item, dbModel);
+                    // Indexes are not available in the model
+                    // GetIndexes(item, dbModel);
                 }
             }
 
@@ -96,7 +132,7 @@ namespace ErikJ.EntityFrameworkCore.Edmx.Scaffolding
             throw new NotImplementedException();
         }
 
-        private void GetColumns(LinqToEdmx.Model.ConceptualV3.EntityType item, DatabaseTable dbTable/*, IReadOnlyDictionary<string, (string storeType, string typeName)> typeAliases, List<TSqlDefaultConstraint> defaultConstraints,*/, LinqToEdmx.EdmxV3 model)
+        private void GetColumnsV3(LinqToEdmx.Model.StorageV3.EntityTypeStore item, DatabaseTable dbTable/*, IReadOnlyDictionary<string, (string storeType, string typeName)> typeAliases, List<TSqlDefaultConstraint> defaultConstraints,*/, LinqToEdmx.EdmxV3 model)
         {
             var tableColumns = item.Properties;
                 //.Where(i => !i.GetProperty<bool>(Column.IsHidden)
@@ -108,27 +144,10 @@ namespace ErikJ.EntityFrameworkCore.Edmx.Scaffolding
 
             foreach (var col in tableColumns)
             {
-                //var def = defaultConstraints.Where(d => d.TargetColumn.First().Name.ToString() == col.Name.ToString()).FirstOrDefault();
-                string storeType = GetStoreType(item, col, model);
-                string systemTypeName = col.Type.ToString();
-
-                //if (col.DataType.First().Name.Parts.Count > 1)
-                //{
-                //    if (typeAliases.TryGetValue($"{col.DataType.First().Name.Parts[0]}.{col.DataType.First().Name.Parts[1]}", out var value))
-                //    {
-                //        storeType = value.storeType;
-                //        systemTypeName = value.typeName;
-                //    }
-                //}
-                //else
-                //{
-                    //var dataTypeName = col.Type.ToString();
-                    //int maxLength = int.Parse(col.MaxLength.ToString());
-                    //storeType = GetStoreType(dataTypeName, maxLength, col.Precision, col.Scale);
-                    //systemTypeName = dataTypeName;
-                //}
-
-                //string defaultValue = def != null ? FilterClrDefaults(systemTypeName, col.Nullable, def.Expression.ToLowerInvariant()) : null;
+                // Default constraints are not available in the model.
+                // var def = defaultConstraints.Where(d => d.TargetColumn.First().Name.ToString() == col.Name.ToString()).FirstOrDefault();
+                string storeType = GetStoreTypeV3(col);
+                string defaultValue = null; //def != null ? FilterClrDefaults(systemTypeName, col.Nullable, def.Expression.ToLowerInvariant()) : null;
 
                 var dbColumn = new DatabaseColumn
                 {
@@ -136,9 +155,13 @@ namespace ErikJ.EntityFrameworkCore.Edmx.Scaffolding
                     Name = col.Name,
                     IsNullable = col.Nullable,
                     StoreType = storeType,
-                    //DefaultValueSql = defaultValue,
-                    //ComputedColumnSql = col.Expression,
-                    //ValueGenerated = col.IsIdentity
+                    DefaultValueSql = defaultValue,
+
+                    // ComputedColumnSQL is not available in the model
+                    // ComputedColumnSql =,
+
+                    // ValueGenerated is not available in the model
+                    // ValueGenerated = col.StoreGeneratedPattern == "Identity"
                     //    ? ValueGenerated.OnAdd
                     //    : storeType == "rowversion"
                     //        ? ValueGenerated.OnAddOrUpdate
@@ -158,61 +181,54 @@ namespace ErikJ.EntityFrameworkCore.Edmx.Scaffolding
                 //    .Where(p => p.Name.Parts[4] == "MS_Description")
                 //    .FirstOrDefault();
 
+                // Description is not available in the model.
                 //dbColumn.Comment = FixExtendedPropertyValue(description?.Value);
 
                 dbTable.Columns.Add(dbColumn);
             }
         }
 
-        private string GetStoreType(LinqToEdmx.Model.ConceptualV3.EntityType item, LinqToEdmx.Model.ConceptualV3.EntityProperty col, EdmxV3 model)
+        // FIXME Add provider string to deal with provider specific data type string
+        private string GetStoreTypeV3(LinqToEdmx.Model.StorageV3.EntityProperty col)
         {
-            var entityContainer = model.GetItems<EntityContainer>();
-            foreach(var container in entityContainer)
+            // SQL Server
+            string dataTypeName = col.Type.ToString();
+
+            if (dataTypeName == "timestamp" || dataTypeName == "uniqueidentifier")
             {
-                if (container.EntitySets.Any(es => es.EntityType.EndsWith(string.Concat("Self.",item.Name), StringComparison.InvariantCultureIgnoreCase)))
-                {
-                    var entitySet = container.EntitySets.Single(es => es.EntityType.EndsWith(string.Concat("Self.", item.Name), StringComparison.InvariantCultureIgnoreCase));
-                    var storeEntitySetName = entitySet.EntityType.Replace("Self.", string.Empty);
-                    // Look through the column mappings to find the conceptual entity
-                    var entityTypeMappings = model.GetItems<EntityTypeMapping>();
-                    foreach (var mapping in entityTypeMappings)
-                    {
-                        var fragment = mapping.MappingFragments[0];
-                        var nameToCompare = string.Empty;
-                        if (string.Compare(fragment.StoreEntitySet, storeEntitySetName, StringComparison.InvariantCultureIgnoreCase) == 0)
-                        {
-                            nameToCompare = item.Name;
-                        }
-
-                        if (string.Compare(fragment.StoreEntitySet, entitySet.Name, StringComparison.InvariantCultureIgnoreCase) == 0)
-                        {
-                            nameToCompare = entitySet.Name;
-                        }
-
-                        if (!string.IsNullOrEmpty(nameToCompare))
-                        {
-                            var storage = fragment.ScalarProperties.First(sp => string.Compare(sp.Name, col.Name, StringComparison.InvariantCultureIgnoreCase) == 0);
-
-                            // And match the corresponding store entity to get the database type
-                            var entityTypeStores = model.GetItems<LinqToEdmx.Model.StorageV3.EntityTypeStore>();
-                            var storeEntity = entityTypeStores.First(ets => string.Compare(ets.Name, nameToCompare, StringComparison.InvariantCultureIgnoreCase) == 0);
-                            var storeType = storeEntity.Properties.First(ets => string.Compare(ets.Name, storage.ColumnName, StringComparison.InvariantCultureIgnoreCase) == 0);
-
-                            return storeType.Type.ToString();
-
-                        }
-                    }
-                }
+                return "rowversion";
             }
 
+            if (dataTypeName == "decimal"
+                || dataTypeName == "numeric")
+            {
+                return $"{dataTypeName}({col.Precision}, {col.Scale})";
+            }
 
-            //var mapping = entityTypeMappings.First(etm => string.Compare(etm.MappingFragments[0].StoreEntitySet, item.Name, StringComparison.InvariantCultureIgnoreCase) == 0);
-            //var fragment = mapping.MappingFragments.First();
-            //var storage = fragment.ScalarProperties.First(sp => string.Compare(sp.Name, col.Name, StringComparison.InvariantCultureIgnoreCase) == 0);
-            throw new InvalidOperationException("Failed to get Store type, this probably indicates a bug.");
-         }
+            if (SQLServerDateTimePrecisionTypes.Contains(dataTypeName)
+                && col.Scale != 7)
+            {
+                return $"{dataTypeName}({col.Scale})";
+            }
 
-        private void GetPrimaryKey(LinqToEdmx.Model.ConceptualV3.EntityType table, DatabaseTable dbTable)
+            if (SQLServerMaxLengthRequiredTypes.Contains(dataTypeName))
+            {
+                // FIXME how is max implemented in SSDL for SQL Server?
+                if (int.Parse(col.MaxLength.ToString()) == -1)
+                {
+                    return $"{dataTypeName}(max)";
+                }
+
+                return $"{dataTypeName}({col.MaxLength})";
+            }
+
+            // Integral type
+            return col.Type.ToString();
+
+            // FIXME PostgreSQL
+        }
+
+        private void GetPrimaryKeyV3(LinqToEdmx.Model.StorageV3.EntityTypeStore table, DatabaseTable dbTable)
         {
             if (table.Key.PropertyRefs.Count() == 0) return;
 
@@ -243,5 +259,70 @@ namespace ErikJ.EntityFrameworkCore.Edmx.Scaffolding
 
         }
 
+        private void GetForeignKeysV3(EdmxV3 model, LinqToEdmx.Model.StorageV3.EntityTypeStore table, DatabaseModel dbModel)
+        {
+            var dbTable = dbModel.Tables
+                .Single(t => t.Name == table.Name
+                && t.Schema == GetSchemaNameV3(model, table.Name));
+
+            var associations = model.GetItems<LinqToEdmx.Model.StorageV3.Association>().Where(a => a.ReferentialConstraint.Principal.Role == table.Name);
+
+            if (associations.Count() == 0)
+                return;
+
+            foreach (var association in associations)
+            {
+                var associationSet = model.GetItems<LinqToEdmx.Model.StorageV3.EntityContainer>().First().AssociationSets.Where(@as => @as.Name == association.Name).SingleOrDefault();
+
+                var entityName = association.ReferentialConstraint.Dependent.Role;
+
+                var foreignTable = dbModel.Tables.SingleOrDefault(t => t.Name == entityName && t.Schema == GetSchemaNameV3(model, entityName));
+                if ( foreignTable == null)
+                {
+                    entityName = associationSet.Ends.Single(e => e.Role == entityName).EntitySet;
+                    foreignTable = dbModel.Tables.SingleOrDefault(t => t.Name == entityName && t.Schema == GetSchemaNameV3(model, entityName));
+                }
+                if (foreignTable == null)
+                {
+                    throw new InvalidOperationException(@$"Unable to get foreign keys for entity with name [{entityName}]. This usually indicates a bug");
+                }
+
+                var foreignKey = new DatabaseForeignKey
+                {
+                    // The name of the foreign key
+                    Name = association.Name,
+                    // The table that contains the foreign key constraint
+                    Table = dbTable,
+                    // The table to which the columns are constrained
+                    PrincipalTable = foreignTable,
+                    // Not available in the model
+                    // OnDelete = ConvertToReferentialAction(fk.DeleteAction)
+                };
+
+                foreach (var fkCol in association.ReferentialConstraint.Principal.PropertyRefs)
+                {
+                    var dbCol = dbTable.Columns
+                        .Single(c => c.Name == fkCol.Name);
+
+                    foreignKey.Columns.Add(dbCol);
+                }
+
+                foreach (var fkCol in association.ReferentialConstraint.Dependent.PropertyRefs)
+                {
+                    var dbCol = foreignTable.Columns
+                        .SingleOrDefault(c => c.Name == fkCol.Name);
+
+                    if (dbCol != null)
+                    {
+                        foreignKey.PrincipalColumns.Add(dbCol);
+                    }
+                }
+
+                if (foreignKey.PrincipalColumns.Count > 0)
+                {
+                    dbTable.ForeignKeys.Add(foreignKey);
+                }
+            }
+        }
     }
 }
