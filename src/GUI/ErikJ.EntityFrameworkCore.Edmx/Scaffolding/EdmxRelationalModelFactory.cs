75a08be32c27d8242859d1a77f50319b7798ba3d
Olivier MATROT <omatrot@free.fr>
2021-01-26 08:31:18 +0100

    The file is included in the project

---
+++ src/GUI/ErikJ.EntityFrameworkCore.Edmx/Scaffolding/EdmxRelationalModelFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Data.Common;
+using Microsoft.EntityFrameworkCore.Scaffolding;
+using Microsoft.EntityFrameworkCore.Scaffolding.Metadata;
+
+namespace ErikJ.EntityFrameworkCore.Edmx.Scaffolding
+{
+    // This is the conceptual part of the model
+    public class EdmxRelationalModelFactory : IDatabaseModelFactory
+    {
+        public DatabaseModel Create(string connectionString, DatabaseModelFactoryOptions options)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DatabaseModel Create(DbConnection connection, DatabaseModelFactoryOptions options)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
