e6929c9ec7c4c56094e71095722c1cedad796b48
stefano mostarda <stesa98@hotmail.com>
2021-02-06 19:12:37 +0100

    Added UI side of database comparison feature

---
+++ src/GUI/EFCorePowerTools/Contracts/ViewModels/ICompareResultViewModel.cs
@@ -0,0 +1,21 @@
+﻿using EFCorePowerTools.Contracts.EventArgs;
+using EFCorePowerTools.Handlers.Compare;
+using EFCorePowerTools.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+
+namespace EFCorePowerTools.Contracts.ViewModels
+{
+    public interface ICompareResultViewModel : IViewModel
+    {
+        event EventHandler<CloseRequestedEventArgs> CloseRequested;
+
+        void AddComparisonResult(IEnumerable<CompareLogModel> connections);
+
+        ObservableCollection<CompareLogItemViewModel> Logs { get; }
+
+        ICommand SetVisibilityCommand { get; }
+    }
+}
---
+++ src/GUI/EFCorePowerTools/Handlers/Compare/CompareType.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EFCorePowerTools.Handlers.Compare
+{
+    public enum CompareType
+    {
+        [Display(Name = "Not set")]
+        NoSet,
+        [Display(Name = "Match anything")]
+        MatchAnything,
+        [Display(Name = "DbContext")]
+        DbContext,
+        [Display(Name = "Entity")]
+        Entity,
+        [Display(Name = "Property")]
+        Property,
+        [Display(Name = "Database")]
+        Database,
+        [Display(Name = "Table")]
+        Table,
+        [Display(Name = "Column")]
+        Column,
+        [Display(Name = "Primary key")]
+        PrimaryKey,
+        [Display(Name = "Foreign key")]
+        ForeignKey,
+        [Display(Name = "Index")]
+        Index
+    }
+
+}
