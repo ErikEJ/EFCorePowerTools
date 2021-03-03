ad75341fc645bdbb83a558385e2ed37e80f1d78b
Erik Ejlskov Jensen <ejlskov@hotmail.com>
2021-03-03 18:00:16 +0100

    Avoid extract of efreveng tool every time Fix missing support for sysname return value with stored procedures

---
+++ src/GUI/NUnitTestCore/SqlServerTypeExtensionsTest.cs
@@ -0,0 +1,64 @@
+﻿using NUnit.Framework;
+using RevEng.Core;
+
+namespace NUnitTestCore
+{
+    [TestFixture]
+    public class SqlServerTypeExtensionsTest
+    {
+        [Test]
+        public void CanParseTypes()
+        {
+            var typeNames = new string[]
+            {
+                "bigint",
+                "binary",
+                "bit",
+                "char",
+                "date",
+                "datetime",
+                "datetime2",
+                "datetimeoffset",
+                "decimal",
+                "float",
+                "geography",
+                "geometry",
+                "hierarchyid",
+                "image",
+                "int",
+                "money",
+                "nchar",
+                "ntext",
+                "numeric",
+                "nvarchar",
+                "real",
+                "smalldatetime",
+                "smallint",
+                "smallmoney",
+                "sql_variant",
+                "sysname",
+                "text",
+                "time",
+                "timestamp",
+                "tinyint",
+                "uniqueidentifier",
+                "varbinary",
+                "varchar",
+                "xml"
+            };
+
+            foreach (var typeName in typeNames)
+            {
+                try
+                {
+                    SqlServerSqlTypeExtensions.GetClrType(typeName, true);
+                    SqlServerSqlTypeExtensions.GetClrType(typeName, false);
+                }
+                catch
+                {
+                    System.Diagnostics.Debug.WriteLine("problem type: " + typeName);
+                }
+            }
+        }
+    }
+}
