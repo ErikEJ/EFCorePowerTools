2b146819b0f0940a19fdc13224d1d58a8a0d6719
Erik Ejlskov Jensen <ErikEJ@users.noreply.github.com>
2021-03-04 20:24:42 +0100

    Add Options page to set cache TTL Update to M.D.S. 2.1.2

--- src/GUI/EFCorePowerTools/Guids.cs
+++ src/GUI/EFCorePowerTools/Guids.cs
@@ -6,6 +6,7 @@ namespace EFCorePowerTools
     {
         public const string guidDbContextPackagePkgString = "f4c4712c-ceae-4803-8e52-0e2049d5de9f";
         public const string guidDbContextPackageCmdSetString = "c769a05d-8d51-4919-bfe6-5f35a0eaf2ba";
+        public const string guidOptionsPageGeneral = "DC8ED597-235B-4CBB-A10E-F3F8C82D89C0";
 
         public static readonly Guid guidDbContextPackageCmdSet = new Guid(guidDbContextPackageCmdSetString);
 
---
+++ src/GUI/EFCorePowerTools/Options/OptionsPageGeneral.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+using System.Runtime.InteropServices;
+using Microsoft.VisualStudio.Shell;
+
+// ReSharper disable once CheckNamespace
+namespace EFCorePowerTools
+{
+    [Guid(GuidList.guidOptionsPageGeneral)]
+    public class OptionsPageGeneral : DialogPage
+    {
+        protected override void OnActivate(CancelEventArgs e)
+        {
+            ModelCacheTimeToLive = Properties.Settings.Default.ModelCacheTimeToLive;
+            base.OnActivate(e);
+        }
+
+        [Category("Reverse Engineer"),
+        DisplayName(@"TTL for DatabaseModel cache"),
+        Description("Sets the time to live value for the cache of persisted database models"),
+        DefaultValue(90)]
+        public int ModelCacheTimeToLive { get; set; }
+
+        protected override void OnApply(PageApplyEventArgs e)
+        {
+            Properties.Settings.Default.ModelCacheTimeToLive = ModelCacheTimeToLive;
+            Properties.Settings.Default.Save();
+            base.OnApply(e);
+        }
+    }
+}
