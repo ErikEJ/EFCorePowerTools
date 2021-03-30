e90ab20ec5f077c04412db4f36b164bfc28144c1
Erik Ejlskov Jensen <ejlskov@hotmail.com>
2021-03-30 11:31:19 +0200

    Store ad-hoc credentials in Credential Manager

---
+++ src/GUI/EFCorePowerTools/DAL/ICredentialStore.cs
@@ -0,0 +1,13 @@
+﻿using EFCorePowerTools.Shared.Models;
+using RevEng.Shared;
+using System.Collections.Generic;
+
+namespace EFCorePowerTools.DAL
+{
+    public interface ICredentialStore
+    {
+        bool DeleteCredential(string name);
+        List<DatabaseConnectionModel> GetStoredDatabaseConnections();
+        bool SaveCredential(DatabaseConnectionModel connectionModel);
+    }
+}
