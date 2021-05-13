07cb46a38af80e4dc2518298ecba7f43611c085a
Erik Ejlskov Jensen <ejlskov@hotmail.com>
2021-05-13 19:11:39 +0200

    Add SqlQuery extension package

---
+++ src/GUI/ErikEJ.EntityFrameworkCore.SqlServer.SqlQuery/DbContextExtensions.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.EntityFrameworkCore
+{
+    public static class DbContextExtensions
+    {
+        public static async Task<List<T>> SqlQueryAsync<T>(this DbContext db, string sql, object[] parameters = null, CancellationToken cancellationToken = default) where T : class
+        {
+            if (parameters is null)
+            {
+                parameters = new object[] { };
+            }
+
+            using (var db2 = new ContextForQueryType<T>(db.Database.GetDbConnection(), db.Database.CurrentTransaction))
+            {
+                db2.Database.SetCommandTimeout(db.Database.GetCommandTimeout());
+                return await db2.Set<T>().FromSqlRaw(sql, parameters).ToListAsync(cancellationToken);
+            }
+        }
+
+        private class ContextForQueryType<T> : DbContext where T : class
+        {
+            private readonly DbConnection connection;
+            private readonly IDbContextTransaction transaction;
+
+            public ContextForQueryType(DbConnection connection, IDbContextTransaction tran)
+            {
+                this.connection = connection;
+                transaction = tran;
+
+                if (tran != null)
+                {
+                    Database.UseTransaction((tran as IInfrastructure<DbTransaction>).Instance);
+                }
+            }
+
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                if (transaction != null)
+                {
+                    optionsBuilder.UseSqlServer(connection);
+                }
+                else
+                {
+                    optionsBuilder.UseSqlServer(connection, options => options.EnableRetryOnFailure());
+                }
+            }
+
+            protected override void OnModelCreating(ModelBuilder modelBuilder)
+            {
+                modelBuilder.Entity<T>().HasNoKey().ToView(null);
+            }
+        }
+    }
+}
