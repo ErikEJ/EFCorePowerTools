ccbb37bf51d8d4bfbd9391a24129a108e332c87a
Erik Ejlskov Jensen <ejlskov@hotmail.com>
2021-05-29 14:36:46 +0200

    Update dependencies

---
+++ test/ScaffoldingTester/DbContextExtensionsTester/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DbContextExtensionsTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
