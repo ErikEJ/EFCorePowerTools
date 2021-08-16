814d659bd8b107442c5dd09152f1253c72aa04dc
Erik Ejlskov Jensen <ejlskov@hotmail.com>
2021-08-16 15:05:03 +0200

    Escape C# keywords in generated code
    fixes #1073

--- src/GUI/RevEng.Core/Routines/SqlServerRoutineScaffolder.cs
+++ src/GUI/RevEng.Core/Routines/SqlServerRoutineScaffolder.cs
@@ -212,8 +212,97 @@ namespace RevEng.Core.Modules
                 }
             }
 
+            if (_keyWords.Contains(name))
+            {
+                name = "@" + name;
+            }
+
             return new Tuple<string, string>(name.Replace(" ", string.Empty), columAttribute);
         }
+
+        private static readonly HashSet<string> _keyWords = new HashSet<string> 
+        {
+"abstract",
+"as",
+"base",
+"bool",
+"break",
+"byte",
+"case",
+"catch",
+"char",
+"checked",
+"class",
+"const",
+"continue",
+"decimal",
+"default",
+"delegate",
+"do",
+"double",
+"else",
+"enum",
+"event",
+"explicit",
+"extern",
+"false",
+"finally",
+"fixed",
+"float",
+"for",
+"foreach",
+"goto",
+"if",
+"implicit",
+"in",
+"int",
+"interface",
+"internal",
+"is",
+"lock",
+"long",
+"namespace",
+"new",
+"null",
+"object",
+"operator",
+"out",
+"override",
+"params",
+"private",
+"protected",
+"public",
+"readonly",
+"ref",
+"return",
+"sbyte",
+"sealed",
+"short",
+"sizeof",
+"stackalloc",
+"static",
+"string",
+"struct",
+"switch",
+"this",
+"throw",
+"true",
+"try",
+"typeof",
+"uint",
+"ulong",
+"unchecked",
+"unsafe",
+"ushort",
+"using",
+"virtual",
+"void",
+"volatile",
+"while",
+        };
+
+
+
 
         private string GenerateUniqueName(Routine routine, RoutineModel model)
         {
