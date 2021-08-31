f76a8cb128cae05d05fa68454f51ec81f13712f0
Erik Ejlskov Jensen <ejlskov@hotmail.com>
2021-08-31 07:28:35 +0200

    Work around kind bug

--- src/GUI/EFCorePowerTools/EFCorePowerToolsPackage.cs
+++ src/GUI/EFCorePowerTools/EFCorePowerToolsPackage.cs
@@ -195,8 +195,10 @@ namespace EFCorePowerTools
             }
 
             menuCommand.Visible =
-                await project.IsKindAsync("{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}") ||
-                await project.IsKindAsync("{9A19103F-16F7-4668-BE54-9A1E7A4F7556}"); // csproj
+                project.FullPath.EndsWith(".csproj", StringComparison.OrdinalIgnoreCase);
+                //TODO Report bug
+                //(await project.IsKindAsync(ProjectTypes.CSHARP)) ||
+                //(await project.IsKindAsync(ProjectTypes.DOTNET_CORE));
 
             return;
         }
