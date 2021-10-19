2ec213bfedfbce0e22e1650fc34d744fe7a95073
Erik Ejlskov Jensen <ejlskov@hotmail.com>
2021-10-19 21:48:16 +0200

    Only show messagebox if needed
    fixes #1147

--- src/GUI/Shared/Handlers/ReverseEngineer/ReverseEngineerHandler.cs
+++ src/GUI/Shared/Handlers/ReverseEngineer/ReverseEngineerHandler.cs
@@ -512,7 +512,10 @@ namespace EFCorePowerTools.Handlers.ReverseEngineer
             await VS.StatusBar.ShowMessageAsync(ReverseEngineerLocale.ReportingResult);
             var errors = reverseEngineerHelper.ReportRevEngErrors(revEngResult, missingProviderPackage);
 
-            await VS.StatusBar.ShowMessageAsync(string.Format(ReverseEngineerLocale.ReverseEngineerCompleted, duration.ToString("h\\:mm\\:ss")));
+            if (errors != ReverseEngineerLocale.ModelGeneratedSuccesfully + Environment.NewLine)
+            {
+                await VS.StatusBar.ShowMessageAsync(string.Format(ReverseEngineerLocale.ReverseEngineerCompleted, duration.ToString("h\\:mm\\:ss")));
+            }
 
             VSHelper.ShowMessage(errors);
 
