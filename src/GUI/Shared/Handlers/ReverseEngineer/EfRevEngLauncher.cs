2e1712f8d3236f7057ee6822a42d05d04a1cb100
Erik Ejlskov Jensen <ErikEJ@users.noreply.github.com>
2022-01-19 16:56:39 +0100

    revert potential break

--- src/GUI/Shared/Handlers/ReverseEngineer/EfRevEngLauncher.cs
+++ src/GUI/Shared/Handlers/ReverseEngineer/EfRevEngLauncher.cs
@@ -247,7 +247,7 @@ namespace EFCorePowerTools.Handlers.ReverseEngineer
 
             if (Directory.Exists(toDir)
                 && File.Exists(Path.Combine(toDir, GetExeName()))
-                && Directory.EnumerateFiles(toDir, "*", SearchOption.TopDirectoryOnly).Count() >= 106)
+                && Directory.EnumerateFiles(toDir, "*", SearchOption.TopDirectoryOnly).Count() >= 99)
             {
                 return fullPath;
             }
