94209b4af2aba6a9fbee97d70e3e918bf6d3bcb7
Erik Ejlskov Jensen <ejlskov@hotmail.com>
2022-05-12 17:18:42 +0200

    fix typo

--- src/GUI/RevEng.Core/IReverseEngineerScaffolder.cs
+++ src/GUI/RevEng.Core/IReverseEngineerScaffolder.cs
@@ -7,7 +7,7 @@ namespace RevEng.Core
     public interface IReverseEngineerScaffolder
     {
         SavedModelFiles GenerateDbContext(ReverseEngineerCommandOptions options, List<string> schemas, string outputContextDir, string modelNamespace, string contextNamespace);
-        SavedModelFiles GenerateFunctions(ReverseEngineerCommandOptions options, ref List<string> errors, string outputContextDir, string modelNamespace, string contextNamespace, bool supportsFunction);
+        SavedModelFiles GenerateFunctions(ReverseEngineerCommandOptions options, ref List<string> errors, string outputContextDir, string modelNamespace, string contextNamespace, bool supportsFunctions);
         SavedModelFiles GenerateStoredProcedures(ReverseEngineerCommandOptions options, ref List<string> errors, string outputContextDir, string modelNamespace, string contextNamespace, bool supportsProcedures);
     }
 }
