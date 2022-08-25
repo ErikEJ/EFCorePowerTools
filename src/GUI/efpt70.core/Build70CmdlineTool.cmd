30b7c14f85941fc097b4eacb0afe946fb5a0acb5
Erik Ejlskov Jensen <ErikEJ@users.noreply.github.com>
2022-08-25 12:46:41 +0200

    Use .NET 6 for EF Core 7 tools (#1502)
    Update reveng tool to latest RC build

--- src/GUI/efpt70.core/Build70CmdlineTool.cmd
+++ src/GUI/efpt70.core/Build70CmdlineTool.cmd
@@ -1,9 +1,9 @@
 
-dotnet publish -o bin\Release\net7.0\publish -f net7.0 -c Release --no-self-contained
+dotnet publish -o bin\Release\net6.0\publish -f net6.0 -c Release --no-self-contained
 
-del bin\Release\net7.0\publish\efpt.exe
+del bin\Release\net6.0\publish\efpt.exe
 
-"C:\Program Files\7-Zip\7z.exe" a efpt70.exe.zip .\bin\Release\net7.0\publish\efpt.*
+"C:\Program Files\7-Zip\7z.exe" a efpt70.exe.zip .\bin\Release\net6.0\publish\efpt.*
 
 move /Y efpt70.exe.zip ..\lib\
 
--- src/GUI/efreveng70/BuildCmdlineTool.cmd
+++ src/GUI/efreveng70/BuildCmdlineTool.cmd
@@ -1,9 +1,9 @@
 
-dotnet publish -o bin\Release\net7.0\publish -f net7.0 -r win-x64 -c Release --no-self-contained
+dotnet publish -o bin\Release\net6.0\publish -f net6.0 -r win-x64 -c Release --no-self-contained
 
 if %errorlevel% equ 1 goto notbuilt
 
-"C:\Program Files\7-Zip\7z.exe" -mm=Deflate -mfb=258 -mpass=15 a efreveng70.exe.zip .\bin\Release\net7.0\publish\*
+"C:\Program Files\7-Zip\7z.exe" -mm=Deflate -mfb=258 -mpass=15 a efreveng70.exe.zip .\bin\Release\net6.0\publish\*
 
 move /Y efreveng70.exe.zip ..\lib\
 
