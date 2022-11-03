8227896ac812810217801445423655e6f3e5c2c6
Erik Ejlskov Jensen <ejlskov@hotmail.com>
2022-11-03 17:03:54 +0100

    EDMX library

---
+++ src/GUI/EdmxRenamingGenerator/EdmxProcessor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using RevEng.Common;
+
+namespace EdmxRenamingGenerator
+{
+    public class EdmxProcessor
+    {
+        public async Task<Model> CreateRenamingModelAsync(string edmxFullPath)
+        {
+            // TODO Implement! :-)
+            return new Model();
+        }
+    }
+}
