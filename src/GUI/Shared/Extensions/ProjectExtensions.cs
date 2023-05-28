21ac253b85ac98e5754a9a1d5e9f5a9766ecbfe1
Erik Ejlskov Jensen <ejlskov@hotmail.com>
2023-05-28 14:07:49 +0200

    Bump

--- src/GUI/Shared/Extensions/ProjectExtensions.cs
+++ src/GUI/Shared/Extensions/ProjectExtensions.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Data;
 using System.IO;
-using System.IO.Packaging;
 using System.Linq;
 using System.Threading.Tasks;
 using Community.VisualStudio.Toolkit;
