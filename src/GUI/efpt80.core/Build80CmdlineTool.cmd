6bcf7b2f1096f486dd13beb3a8e38c07c6c0ec0a
Erik Ejlskov Jensen <ejlskov@hotmail.com>
2023-08-11 14:53:58 +0200

    Update model tool to 8.0 runtime

--- src/GUI/efpt80.core/Build80CmdlineTool.cmd
+++ src/GUI/efpt80.core/Build80CmdlineTool.cmd
@@ -1,9 +1,9 @@
 
-dotnet publish -o bin\Release\net6.0\publish -f net6.0 -c Release --no-self-contained
+dotnet publish -o bin\Release\net8.0\publish -f net8.0 -c Release --no-self-contained
 
-del bin\Release\net6.0\publish\efpt.exe
+del bin\Release\net8.0\publish\efpt.exe
 
-"C:\Program Files\7-Zip\7z.exe" a efpt80.exe.zip .\bin\Release\net6.0\publish\efpt.*
+"C:\Program Files\7-Zip\7z.exe" a efpt80.exe.zip .\bin\Release\net8.0\publish\efpt.*
 
 move /Y efpt80.exe.zip ..\lib\
 
