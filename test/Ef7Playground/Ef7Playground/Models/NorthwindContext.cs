163531b1d03780c81c03079aa2ade2c708f08da4
Erik Ejlskov Jensen <ejlskov@hotmail.com>
2023-10-24 15:32:25 +0200

    fix multiple instances of missing support for ESC and Enter keys fixes #1976

--- test/Ef7Playground/Ef7Playground/Models/NorthwindContext.cs
+++ test/Ef7Playground/Ef7Playground/Models/NorthwindContext.cs
@@ -88,7 +88,7 @@ public partial class NorthwindContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=.\\SQLEXPRESS;Initial Catalog=Northwind;Integrated Security=True;Encrypt=False;Trust Server Certificate=True;Command Timeout=300", x => x
+        => optionsBuilder.UseSqlServer("Data Source=.\\SQLEXPRESS;Initial Catalog=Northwind;Integrated Security=True;Trust Server Certificate=True;Command Timeout=300", x => x
                 .UseNetTopologySuite()
                 .UseHierarchyId());
 
