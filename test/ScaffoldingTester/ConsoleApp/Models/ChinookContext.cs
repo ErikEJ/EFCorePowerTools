79cf26097f3c38080304a8f768f7a501c3e76cc2
Erik Ejlskov Jensen <ejlskov@hotmail.com>
2024-02-05 12:39:29 +0100

    Small tester updates

--- test/ScaffoldingTester/ConsoleApp/Models/ChinookContext.cs
+++ test/ScaffoldingTester/ConsoleApp/Models/ChinookContext.cs
@@ -9,6 +9,10 @@ namespace ConsoleApp.Models;
 
 public partial class ChinookContext : DbContext
 {
+    public ChinookContext()
+    {
+    }
+
     public ChinookContext(DbContextOptions<ChinookContext> options)
         : base(options)
     {
