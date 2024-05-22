b578f507a12defb95d959985f57e63a6ddb32b56
Erik Ejlskov Jensen <ErikEJ@users.noreply.github.com>
2024-05-22 13:51:07 +0200

    Auto create the database in migrations tool (#2347)
    fixes #2344

--- src/Core/efpt70sc.core/Build70ScCmdlineTool.cmd
+++ src/Core/efpt70sc.core/Build70ScCmdlineTool.cmd
@@ -5,6 +5,6 @@ del bin\Release\net7.0\publish\efpt.exe
 
 "C:\Program Files\7-Zip\7z.exe" a efpt70sc.exe.zip .\bin\Release\net7.0\publish\efpt.*
 
-move /Y efpt70sc.exe.zip ..\lib\
+move /Y efpt70sc.exe.zip ..\..\GUI\lib\
 
 pause
--- test/ScaffoldingTester/MigrationsTest/BlogContext.cs
+++ test/ScaffoldingTester/MigrationsTest/BlogContext.cs
@@ -14,7 +14,7 @@ namespace MigrationsTest
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Data Source=.\\SQLEXPRESS;Initial Catalog=Blogs;Integrated Security=True");
+                optionsBuilder.UseSqlServer("Data Source=.\\SQLEXPRESS;Initial Catalog=Blogs;Integrated Security=True;Encrypt=false");
             }
         }
     }
