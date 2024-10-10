c5d4ce7097fbe4bb8e2d20d73b66885acfc49853
Bar Nuri <barnuri@hotmail.com>
2024-10-10 14:56:25 +0300

    Update SqlServerFunctionModelFactory.cs

--- src/Core/RevEng.Core.60/Routines/Functions/SqlServerFunctionModelFactory.cs
+++ src/Core/RevEng.Core.60/Routines/Functions/SqlServerFunctionModelFactory.cs
@@ -48,10 +48,10 @@ SELECT
     c.column_id AS column_ordinal,
     c.is_nullable
 FROM sys.columns c
-left JOIN sys.types AS tu ON c.user_type_id = tu.user_type_id
-left JOIN sys.types AS ts ON c.system_type_id = ts.system_type_id
+inner join sys.types tu ON c.user_type_id = tu.user_type_id 
 inner join sys.objects AS o on o.object_id = c.object_id
 inner JOIN sys.schemas AS s ON o.schema_id = s.schema_id
+LEFT JOIN sys.types ts ON tu.system_type_id = ts.user_type_id
 where o.name = '{module.Name}' and s.name = '{module.Schema}';";
 
 #pragma warning disable CA2100 // Review SQL queries for security vulnerabilities
