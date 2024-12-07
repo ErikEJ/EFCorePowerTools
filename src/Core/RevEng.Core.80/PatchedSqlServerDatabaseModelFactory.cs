cbd79d391db582adc7c583c34afa9a0d26c412cd
Mark Carrington <31017244+MarkMpn@users.noreply.github.com>
2024-12-07 13:06:42 +0000

    Use Dataverse metadata to improve reverse engineering (#2684)
    * Use Dataverse metadata to improve reverse engineering
    
    * Moved dataverse logic to a separate class
    
    * Fixed analyser errors
    
    * Updated file counts
    
    * Updated .NET 9 file count

--- src/Core/RevEng.Core.80/PatchedSqlServerDatabaseModelFactory.cs
+++ src/Core/RevEng.Core.80/PatchedSqlServerDatabaseModelFactory.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Common;
@@ -7,6 +7,7 @@ using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.EntityFrameworkCore.Metadata;
@@ -18,7 +19,12 @@ using Microsoft.EntityFrameworkCore.SqlServer.Internal;
 using Microsoft.EntityFrameworkCore.SqlServer.Metadata.Internal;
 using Microsoft.EntityFrameworkCore.Storage;
 using Microsoft.Extensions.Logging;
+using Microsoft.PowerPlatform.Dataverse.Client;
+using Microsoft.Xrm.Sdk.Messages;
+using Microsoft.Xrm.Sdk.Metadata;
+using Microsoft.Xrm.Sdk.Metadata.Query;
 using RevEng.Common;
+using RevEng.Core;
 
 #nullable enable
 namespace Microsoft.EntityFrameworkCore.SqlServer.Scaffolding.Internal;
@@ -83,6 +89,7 @@ public class PatchedSqlServerDatabaseModelFactory : IDatabaseModelFactory
     private byte? _compatibilityLevel;
     private EngineEdition? _engineEdition;
     private string? _version;
+    private DataverseModelFactoryExtension? _dataverse;
 
     /// <summary>
     ///     This is an internal API that supports the Entity Framework Core infrastructure and not subject to
@@ -123,6 +130,11 @@ public class PatchedSqlServerDatabaseModelFactory : IDatabaseModelFactory
         var databaseModel = new DatabaseModel();
 
         var connectionStartedOpen = connection.State == ConnectionState.Open;
+
+        // Avoid multiple login prompts for Dataverse by authenticating once with the Dataverse service client
+        // and reusing the access token for the TDS Endpoint connection as well.
+        DataverseModelFactoryExtension.TryCreate(connection, out _dataverse);
+
         if (!connectionStartedOpen)
         {
             connection.Open();
@@ -699,13 +711,20 @@ AND [v].[is_date_correlation_view] = 0
         // This is done separately due to MARS property may be turned off
         GetColumns(connection, tables, tableFilterSql, viewFilter, typeAliases, databaseCollation);
 
-        GetIndexes(connection, tables, tableFilterSql);
-
-        GetForeignKeys(connection, tables, tableFilterSql);
-
-        if (SupportsTriggers())
+        if (_dataverse != null)
         {
-            GetTriggers(connection, tables, tableFilterSql);
+            _dataverse.GetDataverseMetadata(tables);
+        }
+        else
+        {
+            GetIndexes(connection, tables, tableFilterSql);
+
+            GetForeignKeys(connection, tables, tableFilterSql);
+
+            if (SupportsTriggers())
+            {
+                GetTriggers(connection, tables, tableFilterSql);
+            }
         }
 
         foreach (var table in tables)
