d3d579e6725e0b09db617fecd15754b0bd8bff8d
Erik Ejlskov Jensen <ErikEJ@users.noreply.github.com>
2024-12-08 16:32:18 +0100

    Shrink payload (#2717)

--- src/Core/efreveng90/BuildCmdlineTool.cmd
+++ src/Core/efreveng90/BuildCmdlineTool.cmd
@@ -12,6 +12,37 @@ del bin\Release\net8.0\x64\publish\Microsoft.SqlServer.Dac.Extensions.dll
 del bin\Release\net8.0\x64\publish\Microsoft.SqlServer.Server.dll
 del bin\Release\net8.0\x64\publish\Microsoft.SqlServer.TransactSql.ScriptDom.dll
 
+rd bin\Release\net8.0\x64\publish\runtimes\android-arm /Q /S
+rd bin\Release\net8.0\x64\publish\runtimes\android-arm64 /Q /S
+rd bin\Release\net8.0\x64\publish\runtimes\android-x64 /Q /S
+rd bin\Release\net8.0\x64\publish\runtimes\android-x86 /Q /S
+rd bin\Release\net8.0\x64\publish\runtimes\browser-wasm /Q /S
+rd bin\Release\net8.0\x64\publish\runtimes\linux /Q /S
+rd bin\Release\net8.0\x64\publish\runtimes\linux-arm /Q /S
+rd bin\Release\net8.0\x64\publish\runtimes\linux-arm64 /Q /S
+rd bin\Release\net8.0\x64\publish\runtimes\linux-armel /Q /S
+rd bin\Release\net8.0\x64\publish\runtimes\linux-mips64 /Q /S
+rd bin\Release\net8.0\x64\publish\runtimes\linux-musl-arm /Q /S
+rd bin\Release\net8.0\x64\publish\runtimes\linux-musl-arm64 /Q /S
+rd bin\Release\net8.0\x64\publish\runtimes\linux-musl-x64 /Q /S
+rd bin\Release\net8.0\x64\publish\runtimes\linux-ppc64le /Q /S
+rd bin\Release\net8.0\x64\publish\runtimes\linux-s390x /Q /S
+rd bin\Release\net8.0\x64\publish\runtimes\linux-x64 /Q /S
+rd bin\Release\net8.0\x64\publish\runtimes\linux-x86 /Q /S
+rd bin\Release\net8.0\x64\publish\runtimes\ios-arm /Q /S
+rd bin\Release\net8.0\x64\publish\runtimes\ios-arm64 /Q /S
+rd bin\Release\net8.0\x64\publish\runtimes\ios-armv7s /Q /S
+rd bin\Release\net8.0\x64\publish\runtimes\iossimulator-arm64 /Q /S
+rd bin\Release\net8.0\x64\publish\runtimes\iossimulator-x64 /Q /S
+rd bin\Release\net8.0\x64\publish\runtimes\maccatalyst-arm64 /Q /S
+rd bin\Release\net8.0\x64\publish\runtimes\maccatalyst-x64 /Q /S
+rd bin\Release\net8.0\x64\publish\runtimes\osx /Q /S
+rd bin\Release\net8.0\x64\publish\runtimes\osx-arm64 /Q /S 
+rd bin\Release\net8.0\x64\publish\runtimes\osx-x64 /Q /S
+rd bin\Release\net8.0\x64\publish\runtimes\tvos-arm64 /Q /S
+rd bin\Release\net8.0\x64\publish\runtimes\tvossimulator-x64 /Q /S
+rd bin\Release\net8.0\x64\publish\runtimes\unix /Q /S
+
 "C:\Program Files\7-Zip\7z.exe" -mm=Deflate -mfb=258 -mpass=15 a efreveng90.exe.zip .\bin\Release\net8.0\x64\publish\*
 
 move /Y efreveng90.exe.zip ..\..\GUI\lib\
