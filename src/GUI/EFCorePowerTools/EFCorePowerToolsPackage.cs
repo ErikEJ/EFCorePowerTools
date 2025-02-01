dd551fcf1441180a727286fd26ab22318505bbb3
William Kratochvil <billkrat@adventuresontheedge.net>
2025-02-01 05:31:02 -0600

    Removed the view model associated with the EFCoreModelDialog [orphaned by wizard work]

--- src/GUI/EFCorePowerTools/EFCorePowerToolsPackage.cs
+++ src/GUI/EFCorePowerTools/EFCorePowerToolsPackage.cs
@@ -1069,7 +1069,6 @@ namespace EFCorePowerTools
                     .AddSingleton<Func<ITableInformationViewModel>>(provider => () => new TableInformationViewModel(provider.GetService<IMessenger>()))
                     .AddSingleton<Func<IColumnInformationViewModel>>(provider => () => new ColumnInformationViewModel(provider.GetService<IMessenger>()))
                     .AddSingleton<Func<IColumnChildrenViewModel>>(provider => () => new ColumnChildrenViewModel(provider.GetService<IMessenger>()))
-                    .AddTransient<IModelingOptionsViewModel, ModelingOptionsViewModel>()
                     .AddTransient<IPickSchemasViewModel, PickSchemasViewModel>()
                     .AddTransient<IAdvancedModelingOptionsViewModel, AdvancedModelingOptionsViewModel>()
                     .AddTransient<IObjectTreeViewModel, ObjectTreeViewModel>()
