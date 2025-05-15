7c8c4522dde0f04ac60aafd785bd6bdedb27e78c
Erik Ejlskov Jensen <eej@delegate.dk>
2025-05-15 11:18:10 +0200

    fix build

---
+++ src/Core/RevEng.Core.80/ConnectionStringResolver.cs
@@ -0,0 +1,108 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using FirebirdSql.Data.FirebirdClient;
+using Microsoft.Data.SqlClient;
+using Microsoft.Data.Sqlite;
+using MySqlConnector;
+using Npgsql;
+using Oracle.ManagedDataAccess.Client;
+
+namespace RevEng.Core
+{
+    public class ConnectionStringResolver
+    {
+        private readonly string connectionString;
+        private readonly bool isDacpac;
+
+        public ConnectionStringResolver(string connectionString)
+        {
+            ArgumentNullException.ThrowIfNull(connectionString);
+
+            this.connectionString = connectionString;
+
+            if (connectionString.EndsWith(".dacpac", StringComparison.OrdinalIgnoreCase))
+            {
+                isDacpac = true;
+                var database = Path.GetFileNameWithoutExtension(connectionString);
+                this.connectionString = $"Data Source=(local);Initial Catalog={database};Integrated Security=true;Encrypt=false";
+            }
+        }
+
+#pragma warning disable CA1031
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Minor Code Smell", "S1481:Unused local variables should be removed", Justification = "Part of logic")]
+        public IList<string> ResolveAlias()
+        {
+            var aliases = new List<string>();
+
+            if (isDacpac)
+            {
+                aliases.Add("mssql");
+                return aliases;
+            }
+
+            try
+            {
+                var a = new SqlConnectionStringBuilder(connectionString);
+                aliases.Add("mssql");
+            }
+            catch
+            {
+                // Ignore
+            }
+
+            try
+            {
+                var a = new NpgsqlConnectionStringBuilder(connectionString);
+                aliases.Add("postgres");
+            }
+            catch
+            {
+                // Ignore
+            }
+
+            try
+            {
+                var a = new SqliteConnectionStringBuilder(connectionString);
+                aliases.Add("sqlite");
+            }
+            catch
+            {
+                // Ignore
+            }
+
+            try
+            {
+                var a = new OracleConnectionStringBuilder(connectionString);
+                aliases.Add("oracle");
+            }
+            catch
+            {
+                // Ignore
+            }
+
+            try
+            {
+                var a = new MySqlConnectionStringBuilder(connectionString);
+                aliases.Add("mysql");
+            }
+            catch
+            {
+                // Ignore
+            }
+
+            try
+            {
+                var a = new FbConnectionStringBuilder(connectionString);
+                aliases.Add("firebird");
+            }
+            catch
+            {
+                // Ignore
+            }
+
+            return aliases;
+        }
+#pragma warning restore CA1031
+    }
+}
