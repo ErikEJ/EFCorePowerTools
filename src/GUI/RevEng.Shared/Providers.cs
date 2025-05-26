afab6c351e9a26a839a873190e5a7ceeb34237d4
Erik Ejlskov Jensen <eej@delegate.dk>
2025-05-26 10:57:20 +0200

    Update Oracle provider to .23.80

--- src/GUI/RevEng.Shared/Providers.cs
+++ src/GUI/RevEng.Shared/Providers.cs
@@ -432,10 +432,10 @@ namespace RevEng.Common
                         pkgVersion = "6.21.160";
                         break;
                     case CodeGenerationMode.EFCore8:
-                        pkgVersion = "8.23.60";
+                        pkgVersion = "8.23.80";
                         break;
                     case CodeGenerationMode.EFCore9:
-                        pkgVersion = "9.23.60";
+                        pkgVersion = "9.23.80";
                         break;
 
                     default: throw new NotImplementedException();
