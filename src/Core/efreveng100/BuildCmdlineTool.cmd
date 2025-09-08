a2ca7635ee2620392957e85d486afdb5e973bc6e
Erik Ejlskov Jensen <eej@delegate.dk>
2025-09-08 08:29:31 +0200

    Build tweaks

--- src/Core/efreveng100/BuildCmdlineTool.cmd
+++ src/Core/efreveng100/BuildCmdlineTool.cmd
@@ -26,6 +26,9 @@ rd bin\Release\net10.0\x64\publish\runtimes\linux-mips64 /Q /S
 rd bin\Release\net10.0\x64\publish\runtimes\linux-musl-arm /Q /S
 rd bin\Release\net10.0\x64\publish\runtimes\linux-musl-arm64 /Q /S
 rd bin\Release\net10.0\x64\publish\runtimes\linux-musl-s390x /Q /S
+rd bin\Release\net10.0\x64\publish\runtimes\linux-musl-s390x /Q /S
+rd bin\Release\net10.0\x64\publish\runtimes\linux-musl-riscv64 /Q /S
+rd bin\Release\net10.0\x64\publish\runtimes\linux-riscv64 /Q /S
 rd bin\Release\net10.0\x64\publish\runtimes\linux-musl-x64 /Q /S
 rd bin\Release\net10.0\x64\publish\runtimes\linux-ppc64le /Q /S
 rd bin\Release\net10.0\x64\publish\runtimes\linux-s390x /Q /S
