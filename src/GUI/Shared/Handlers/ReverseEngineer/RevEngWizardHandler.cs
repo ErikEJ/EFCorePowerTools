25413d9138718fbf0fb3a2665e84dbdc8b893755
Erik Ejlskov Jensen <ErikEJ@users.noreply.github.com>
2026-02-27 15:30:07 +0100

    Revert (#3322)
    * Revert "Always build the database project if needed (#3293)"
    
    This reverts commit 8125c069331bc5356e9d47a2570a1614910439e0.
    
    * fix nullable params

--- src/GUI/Shared/Handlers/ReverseEngineer/RevEngWizardHandler.cs
+++ src/GUI/Shared/Handlers/ReverseEngineer/RevEngWizardHandler.cs
@@ -646,7 +646,7 @@ namespace EFCorePowerTools.Handlers.ReverseEngineer
                 options.ConnectionString = dbInfo.ConnectionString;
                 options.DatabaseType = dbInfo.DatabaseType;
 
-                options.Dacpac = await SqlProjHelper.BuildSqlProjectAsync(options.Dacpac);
+                options.Dacpac = await SqlProjHelper.BuildSqlProjectAsync(options.Dacpac, mustBuild: false);
                 if (string.IsNullOrEmpty(options.Dacpac))
                 {
                     VSHelper.ShowMessage(ReverseEngineerLocale.UnableToBuildSelectedDatabaseProject);
